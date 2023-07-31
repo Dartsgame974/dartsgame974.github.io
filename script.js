4b8e97e40ecf5dd209c06cf1db96e392a61a5156
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-07-31 17:59:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('resizeButton').addEventListener('click', function () {
-    var inputFile = document.getElementById('inputFile').files[0];
+    var inputFiles = document.getElementById('inputFile').files;
     var selectedOption = document.querySelector('input[name="resizeOption"]:checked').value;
 
     var resizeOptions;
@@ -11,31 +11,52 @@ document.getElementById('resizeButton').addEventListener('click', function () {
         resizeOptions = [28, 56, 112];
     }
 
-    if (inputFile) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            var img = new Image();
-            img.src = e.target.result;
-            img.onload = function () {
-                var canvas = document.createElement('canvas');
-                var ctx = canvas.getContext('2d');
+    if (inputFiles.length > 0) {
+        var zip = new JSZip();
+        var filePromises = [];
 
-                document.getElementById('outputContainer').innerHTML = '';
+        for (var j = 0; j < inputFiles.length; j++) {
+            var inputFile = inputFiles[j];
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var img = new Image();
+                img.src = e.target.result;
+                img.onload = function () {
+                    var canvas = document.createElement('canvas');
+                    var ctx = canvas.getContext('2d');
 
-                for (var i = 0; i < resizeOptions.length; i++) {
-                    var resizeOption = resizeOptions[i];
-                    canvas.width = resizeOption;
-                    canvas.height = resizeOption;
-                    ctx.clearRect(0, 0, resizeOption, resizeOption);
-                    ctx.drawImage(img, 0, 0, resizeOption, resizeOption);
-                    var resizedData = canvas.toDataURL('image/png');
-                    var resizedImg = document.createElement('img');
-                    resizedImg.src = resizedData;
-                    document.getElementById('outputContainer').appendChild(resizedImg);
-                }
+                    for (var i = 0; i < resizeOptions.length; i++) {
+                        var resizeOption = resizeOptions[i];
+                        canvas.width = resizeOption;
+                        canvas.height = resizeOption;
+                        ctx.clearRect(0, 0, resizeOption, resizeOption);
+                        ctx.drawImage(img, 0, 0, resizeOption, resizeOption);
+                        var resizedData = canvas.toDataURL('image/png');
+
+                        // Ajouter les images redimensionnées dans le fichier ZIP
+                        var imgFileName = inputFile.name + "_" + resizeOption + "x" + resizeOption + ".png";
+                        zip.file(imgFileName, resizedData.substr(resizedData.indexOf(',') + 1), { base64: true });
+                    }
+                };
             };
-        };
 
-        reader.readAsDataURL(inputFile);
+            filePromises.push(new Promise(function(resolve) {
+                reader.readAsDataURL(inputFile);
+                reader.onloadend = resolve;
+            }));
+        }
+
+        Promise.all(filePromises).then(function() {
+            // Activer le bouton de téléchargement et définir l'action de téléchargement
+            document.getElementById('downloadButton').disabled = false;
+            document.getElementById('downloadButton').addEventListener('click', function() {
+                zip.generateAsync({ type: "blob" }).then(function (content) {
+                    var link = document.createElement("a");
+                    link.href = URL.createObjectURL(content);
+                    link.download = "images.zip";
+                    link.click();
+                });
+            });
+        });
     }
 });
