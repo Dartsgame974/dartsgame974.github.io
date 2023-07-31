c6b247dce46e02b2aaac70f7d4db9c5036886003
Dartsgame974 <80755313+Dartsgame974@users.noreply.github.com>
2023-07-31 19:26:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,105 @@
-document.getElementById('resizeButton').addEventListener('click', function () {
-    var inputFiles = document.getElementById('inputFile').files;
-    var selectedOption = document.querySelector('input[name="resizeOption"]:checked').value;
+// Fonction pour redimensionner une image
+function redimensionnerImage(image, taille) {
+  var canvas = document.createElement("canvas");
+  var context = canvas.getContext("2d");
+  canvas.width = taille;
+  canvas.height = taille;
+  context.drawImage(image, 0, 0, taille, taille);
+  return canvas.toDataURL("image/png");
+}
 
-    var resizeOptions;
-    if (selectedOption === 'twitch') {
-        resizeOptions = [72, 36, 18];
-    } else if (selectedOption === 'discord') {
-        resizeOptions = [256];
-    } else if (selectedOption === 'chaine') {
-        resizeOptions = [28, 56, 112];
-    }
+// Liste pour stocker les images chargées
+var imagesChargees = [];
 
-    if (inputFiles.length > 0) {
-        var zip = new JSZip();
-        var filePromises = [];
+// Fonction pour gérer le chargement des fichiers
+function gererChargementFichiers(event) {
+  imagesChargees = []; // Réinitialiser la liste des images chargées
 
-        for (var j = 0; j < inputFiles.length; j++) {
-            var inputFile = inputFiles[j];
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var img = new Image();
-                img.src = e.target.result;
-                img.onload = function () {
-                    var canvas = document.createElement('canvas');
-                    var ctx = canvas.getContext('2d');
+  var files = event.target.files;
+  var previewContainer = document.getElementById("previewContainer");
+  previewContainer.innerHTML = ""; // Effacer tout contenu précédent
 
-                    for (var i = 0; i < resizeOptions.length; i++) {
-                        var resizeOption = resizeOptions[i];
-                        canvas.width = resizeOption;
-                        canvas.height = resizeOption;
-                        ctx.clearRect(0, 0, resizeOption, resizeOption);
-                        ctx.drawImage(img, 0, 0, resizeOption, resizeOption);
-                        var resizedData = canvas.toDataURL('image/png');
+  for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    var reader = new FileReader();
 
-                        // Ajouter les images redimensionnées dans le fichier ZIP
-                        var imgFileName = inputFile.name + "_" + resizeOption + "x" + resizeOption + ".png";
-                        zip.file(imgFileName, resizedData.substr(resizedData.indexOf(',') + 1), { base64: true });
-                    }
-                };
-            };
+    // Vérifier si le fichier est une image PNG
+    if (file.type === "image/png") {
+      reader.onload = function (event) {
+        var image = new Image();
+        image.src = event.target.result;
 
-            filePromises.push(new Promise(function(resolve) {
-                reader.readAsDataURL(inputFile);
-                reader.onloadend = resolve;
-            }));
-        }
+        // Redimensionner l'image aux tailles requises
+        var image_72 = redimensionnerImage(image, 72);
+        var image_36 = redimensionnerImage(image, 36);
+        var image_18 = redimensionnerImage(image, 18);
 
-        Promise.all(filePromises).then(function() {
-            // Activer le bouton de téléchargement et définir l'action de téléchargement
-            var downloadButton = document.getElementById('downloadButton');
-            downloadButton.disabled = false;
-            downloadButton.addEventListener('click', function() {
-                zip.generateAsync({ type: "blob" }).then(function (content) {
-                    var link = document.createElement("a");
-                    link.href = URL.createObjectURL(content);
-                    link.download = "images.zip";
-                    link.click();
-                });
-            });
+        imagesChargees.push({
+          dataUrl_72: image_72,
+          dataUrl_36: image_36,
+          dataUrl_18: image_18,
         });
+
+        // Vérifier si toutes les images ont été chargées avant d'afficher l'aperçu et activer le bouton "Télécharger ZIP"
+        if (imagesChargees.length === files.length) {
+          imagesChargees.forEach((image, index) => {
+            var previewImage_72 = document.createElement("img");
+            previewImage_72.src = image.dataUrl_72;
+            previewImage_72.style.maxWidth = "100px";
+            previewImage_72.style.maxHeight = "100px";
+            previewContainer.appendChild(previewImage_72);
+
+            var previewImage_36 = document.createElement("img");
+            previewImage_36.src = image.dataUrl_36;
+            previewImage_36.style.maxWidth = "100px";
+            previewImage_36.style.maxHeight = "100px";
+            previewContainer.appendChild(previewImage_36);
+
+            var previewImage_18 = document.createElement("img");
+            previewImage_18.src = image.dataUrl_18;
+            previewImage_18.style.maxWidth = "100px";
+            previewImage_18.style.maxHeight = "100px";
+            previewContainer.appendChild(previewImage_18);
+          });
+
+          document.getElementById("downloadButton").disabled = false;
+        }
+      };
+
+      reader.readAsDataURL(file);
+    } else {
+      alert("Le fichier " + file.name + " n'est pas une image PNG valide.");
     }
-});
+  }
+}
+
+// Fonction pour télécharger les images redimensionnées sous forme de ZIP
+function telechargerZip() {
+  if (imagesChargees.length === 0) {
+    alert("Aucune image chargée.");
+    return;
+  }
+
+  var zip = new JSZip();
+
+  imagesChargees.forEach((image, index) => {
+    var nomFichier_72 = "image_" + (index + 1) + "_72.png";
+    zip.file(nomFichier_72, image.dataUrl_72.split("base64,")[1], { base64: true });
+
+    var nomFichier_36 = "image_" + (index + 1) + "_36.png";
+    zip.file(nomFichier_36, image.dataUrl_36.split("base64,")[1], { base64: true });
+
+    var nomFichier_18 = "image_" + (index + 1) + "_18.png";
+    zip.file(nomFichier_18, image.dataUrl_18.split("base64,")[1], { base64: true });
+  });
+
+  zip.generateAsync({ type: "blob" }).then(function (contenu) {
+    saveAs(contenu, "images_redimensionnees.zip");
+  });
+}
+
+// Écouter l'événement de chargement de fichiers
+document.getElementById("inputFile").addEventListener("change", gererChargementFichiers);
+
+// Écouter l'événement du bouton "Redimensionner"
+document.getElementById("downloadButton").addEventListener("click", telechargerZip);
