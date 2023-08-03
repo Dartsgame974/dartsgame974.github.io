069215997e2290280ca8ab476d413dc742f9832b
Dartsgame <80755313+Dartsgame974@users.noreply.github.com>
2023-08-03 20:59:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,89 @@
-function parcourirDossier() {
-    // Ouvre une fenêtre de sélection de fichiers
-    var input = document.createElement('input');
-    input.type = 'file';
-    input.accept = 'image/*';
-    input.multiple = true;
-    input.addEventListener('change', function(event) {
-        var files = event.target.files;
-        var chemin_acces = "";
-        for (var i = 0; i < files.length; i++) {
-            chemin_acces += files[i].name + "; ";
+const dropArea = document.getElementById('dropArea');
+const fileInput = document.getElementById('fileInput');
+const previewContainer = document.getElementById('previewContainer');
+
+dropArea.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    dropArea.classList.add('active');
+});
+
+dropArea.addEventListener('dragleave', () => {
+    dropArea.classList.remove('active');
+});
+
+dropArea.addEventListener('drop', (event) => {
+    event.preventDefault();
+    dropArea.classList.remove('active');
+    const files = event.dataTransfer.files;
+    handleFiles(files);
+});
+
+fileInput.addEventListener('change', (event) => {
+    const files = event.target.files;
+    handleFiles(files);
+});
+
+function handleFiles(files) {
+    previewContainer.innerHTML = '';
+    if (files.length === 0) {
+        dropArea.classList.add('empty');
+    } else {
+        dropArea.classList.remove('empty');
+        for (const file of files) {
+            const reader = new FileReader();
+            reader.onload = (event) => {
+                const img = document.createElement('img');
+                img.src = event.target.result;
+                const previewItem = document.createElement('div');
+                previewItem.classList.add('preview-item');
+                previewItem.appendChild(img);
+                previewContainer.appendChild(previewItem);
+            };
+            reader.readAsDataURL(file);
         }
-        document.querySelector(".textbox").value = chemin_acces;
-    });
-    input.click();
+    }
 }
 
-function redimensionnerImage(chemin_image, taille) {
-    // Votre code pour redimensionner une image ici
-    console.log("Redimensionner l'image : ", chemin_image, "à la taille : ", taille);
-}
+function resizeImagesAndCreateZip() {
+    const resolutions = document.querySelectorAll('input[name="resolutions"]:checked');
+    const files = fileInput.files;
 
-function redimensionnerImages() {
-    var chemin_acces = document.querySelector(".textbox").value;
-    if (!chemin_acces) {
-        alert("Veuillez entrer le chemin d'accès d'un répertoire contenant des images à redimensionner.");
+    if (resolutions.length === 0 || files.length === 0) {
+        alert("Veuillez sélectionner des résolutions et des images à redimensionner.");
         return;
     }
 
-    var options_selectionnees = {
-        Twitch: document.getElementById("bits").checked,
-        Discord: document.getElementById("channelpoints").checked
-        // Ajoutez d'autres options si nécessaire...
-    };
+    const zip = new JSZip();
 
-    if (!Object.values(options_selectionnees).some(Boolean)) {
-        alert("Aucune option de redimensionnement sélectionnée.");
-        return;
+    for (const file of files) {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            const img = new Image();
+            img.src = event.target.result;
+
+            img.onload = () => {
+                for (const resolution of resolutions) {
+                    const [width, height] = resolution.value.split('x');
+                    const canvas = document.createElement('canvas');
+                    canvas.width = width;
+                    canvas.height = height;
+
+                    const ctx = canvas.getContext('2d');
+                    ctx.drawImage(img, 0, 0, width, height);
+
+                    const dataUrl = canvas.toDataURL('image/jpeg', 0.8);
+                    zip.file(`${file.name}_${resolution.value}.jpg`, dataUrl.split(',')[1], { base64: true });
+                }
+            };
+        };
+        reader.readAsDataURL(file);
     }
 
-    // Votre code pour redimensionner les images ici
-    console.log("Redimensionner les images...");
-    // Remplacez cette ligne avec la logique de redimensionnement réelle
-
-    // Exemple de redimensionnement d'une image
-    if (options_selectionnees["Twitch"]) {
-        redimensionnerImage(chemin_acces + "/exemple.png", 72);
-        redimensionnerImage(chemin_acces + "/exemple.png", 36);
-        redimensionnerImage(chemin_acces + "/exemple.png", 18);
-    }
-
-    if (options_selectionnees["Discord"]) {
-        redimensionnerImage(chemin_acces + "/exemple.png", 256);
-    }
+    zip.generateAsync({ type: 'blob' })
+        .then((content) => {
+            saveAs(content, 'images_resized.zip');
+        });
 }
 
-document.getElementById("redimensionner").addEventListener("click", redimensionnerImages);
+const resizeButton = document.getElementById('resizeButton');
+resizeButton.addEventListener('click', resizeImagesAndCreateZip);
