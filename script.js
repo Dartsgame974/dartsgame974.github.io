5b432745bd9b0deb5253405454870eaa43b36556
Dartsgame <80755313+Dartsgame974@users.noreply.github.com>
2023-08-03 21:07:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,109 +4,14 @@
     <meta charset="UTF-8">
     <title>Redimensionnement d'Images et Création d'un Fichier Zip</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-    <style>
-        body {
-            font-family: 'Montserrat', sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #1a1a1a; /* Couleur de fond en mode sombre */
-            color: #ffffff; /* Couleur du texte en mode sombre */
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
-
-        h1 {
-            text-align: center;
-            padding: 20px;
-        }
-
-        .drop-area {
-            border: 2px dashed #007bff; /* Couleur de la zone de drag and drop en mode sombre */
-            padding: 20px;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: border-color 0.3s;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-        }
-
-        .drop-area:hover {
-            border-color: #0056b3; /* Couleur de la zone de drag and drop en mode sombre lors du survol */
-        }
-
-        p {
-            margin: 5px 0;
-        }
-
-        label {
-            display: block;
-            margin: 5px 0;
-        }
-
-        button {
-            display: block;
-            margin: 20px auto;
-            padding: 10px 20px;
-            background-color: #007bff; /* Couleur du bouton en mode sombre */
-            color: #ffffff; /* Couleur du texte du bouton en mode sombre */
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        button:hover {
-            background-color: #0056b3; /* Couleur du bouton en mode sombre lors du survol */
-        }
-
-        /* Cacher le champ de fichier d'origine */
-        input[type="file"] {
-            display: none;
-        }
-
-        /* Styler la zone de drag and drop lorsqu'un fichier est survolé */
-        .drop-area.active {
-            border-color: #0056b3; /* Couleur de la zone de drag and drop en mode sombre lors du survol avec un fichier */
-        }
-
-        /* Afficher le texte "Aucun fichier choisi" lorsque aucun fichier n'a été sélectionné */
-        .drop-area.empty:after {
-            content: "Faites glisser et déposez les fichiers ici ou cliquez pour sélectionner des images.";
-            font-size: 14px;
-            color: #ffffff; /* Couleur du texte en mode sombre */
-        }
-
-        /* Afficher la prévisualisation des images chargées */
-        .preview-container {
-            display: flex;
-            flex-wrap: wrap;
-            margin-top: 20px;
-        }
-
-        .preview-item {
-            width: 200px;
-            margin: 10px;
-            border: 1px solid #007bff; /* Couleur de la bordure en mode sombre */
-            padding: 5px;
-            border-radius: 5px;
-        }
-
-        .preview-item img {
-            max-width: 100%;
-            height: auto;
-        }
-    </style>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h1>Redimensionnement d'Images</h1>
-    <label for="fileInput" class="drop-area empty" id="dropArea">
+    <div class="drop-area" id="dropArea">
         <p>Faites glisser et déposez les fichiers ici ou cliquez pour sélectionner des images.</p>
-    </label>
-    <input type="file" id="fileInput" multiple>
+        <input type="file" id="fileInput" multiple>
+    </div>
     <div class="preview-container" id="previewContainer"></div>
     <p>Sélectionnez les résolutions :</p>
     <label>
