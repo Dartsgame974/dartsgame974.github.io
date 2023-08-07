55fadaf7a61455e1e1d480796e5727850363f587
Dartsgame <80755313+Dartsgame974@users.noreply.github.com>
2023-08-08 00:34:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,152 +1,77 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Redimensionnement d'Images et Création d'un Fichier Zip</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-    <style>
-        body {
-            font-family: 'Montserrat', sans-serif;
-            margin: 0;
-            padding: 0;
-            background-color: #1a1a1a; /* Couleur de fond en mode sombre */
-            color: #ffffff; /* Couleur du texte en mode sombre */
-        }
+// Récupérer les éléments du DOM
+const upload = document.getElementById('upload');
+const previews = document.querySelector('.previews');
+const resizeBtn = document.getElementById('resize');
 
-        h1 {
-            text-align: center;
-            padding: 20px;
-        }
+// Résolutions par plateforme 
+const sizes = {
+  twitch: [72, 36, 18],
+  discord: [256],
+  moji: [28, 56, 112]
+};
 
-        input[type="file"] {
-            margin: 10px 0;
-        }
+// Écouter le chargement de fichiers
+upload.addEventListener('change', () => {
+  previews.innerHTML = ''; // Réinitialiser
 
-        p {
-            margin: 5px 0;
-            color: #ffffff; /* Couleur du texte en mode sombre */
-        }
+  const files = upload.files;
+  for(let i = 0; i < files.length; i++) {
+    const file = files[i];
 
-        label {
-            display: block;
-            margin: 5px 0;
-        }
+    // Créer l'élément img
+    const img = document.createElement('img');
+    img.src = URL.createObjectURL(file);
+    img.className = 'preview';
 
-        button {
-            display: block;
-            margin: 20px auto;
-            padding: 10px 20px;
-            background-color: #007bff; /* Couleur du bouton en mode sombre */
-            color: #ffffff; /* Couleur du texte du bouton en mode sombre */
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-        }
+    // Ajouter au DOM
+    previews.appendChild(img); 
+  }
+});
 
-        button:hover {
-            background-color: #0056b3; /* Couleur du bouton en mode sombre lors du survol */
-        }
-    </style>
-</head>
-<body>
-    <h1>Redimensionnement d'Images</h1>
-    <input type="file" id="fileInput" multiple>
-    <br>
-    <p>Sélectionnez les résolutions :</p>
-    <label>
-        <input type="checkbox" name="resolutions" value="72x72">Twitch 72x72
-    </label>
-    <label>
-        <input type="checkbox" name="resolutions" value="36x36">Twitch 36x36
-    </label>
-    <label>
-        <input type="checkbox" name="resolutions" value="18x18">Twitch 18x18
-    </label>
-    <br>
-    <label>
-        <input type="checkbox" name="resolutions" value="256x256">Discord 256x256
-    </label>
-    <br>
-    <label>
-        <input type="checkbox" name="resolutions" value="28x28">Points de chaîne 28x28
-    </label>
-    <label>
-        <input type="checkbox" name="resolutions" value="56x56">Points de chaîne 56x56
-    </label>
-    <label>
-        <input type="checkbox" name="resolutions" value="112x112">Points de chaîne 112x112
-    </label>
-    <br>
-    <button id="resizeButton">Redimensionner</button>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
-    <script>
-        // Fonction pour redimensionner une image dans une résolution spécifiée
-        function resizeImage(image, resolution) {
-            const canvas = document.createElement('canvas');
-            canvas.width = resolution;
-            canvas.height = resolution;
-            const ctx = canvas.getContext('2d');
-            ctx.drawImage(image, 0, 0, resolution, resolution);
-            return canvas.toDataURL('image/jpeg', 1.0);
-        }
+// Écouter le clic sur le bouton
+resizeBtn.addEventListener('click', () => {
+  // Récupérer les options cochées
+  const options = getCheckedOptions();
 
-        // Fonction pour créer un fichier zip à partir des images redimensionnées
-        function createZip(imagesData) {
-            const zip = new JSZip();
-            imagesData.forEach((imageData, index) => {
-                const fileName = `image_${index + 1}_${imageData.resolution}.jpg`;
-                zip.file(fileName, imageData.data.split(',')[1], { base64: true });
-            });
-            zip.generateAsync({ type: 'blob' }).then((content) => {
-                saveAs(content, 'images_resized.zip');
-            });
-        }
+  // Redimensionner et zipper
+  zipImages(upload.files, options);
+});
 
-        // Fonction appelée lorsque le bouton "Redimensionner" est cliqué
-        function onResizeButtonClick() {
-            const selectedResolutions = Array.from(document.querySelectorAll('input[name="resolutions"]:checked'))
-                .map(input => {
-                    const [width, height] = input.value.split('x').map(Number);
-                    return { resolution: input.value, width, height };
-                });
+// Utilitaires
 
-            const files = document.getElementById('fileInput').files;
-            if (files.length === 0 || selectedResolutions.length === 0) {
-                alert('Veuillez sélectionner au moins un fichier et une résolution.');
-                return;
-            }
+// Récupérer les options cochées  
+function getCheckedOptions() {
+  const checked = [];
 
-            const imagesData = [];
+  const twitch = document.getElementById('twitch');
+  const discord = document.getElementById('discord');
+  const moji = document.getElementById('moji');
 
-            // Boucle pour traiter chaque fichier sélectionné
-            for (let i = 0; i < files.length; i++) {
-                const file = files[i];
-                const reader = new FileReader();
+  if (twitch.checked) {
+    checked.push({
+      name: 'twitch',
+      sizes: sizes.twitch
+    });
+  }
 
-                reader.onload = function(event) {
-                    const image = new Image();
-                    image.src = event.target.result;
-                    image.onload = function() {
-                        // Boucle pour redimensionner l'image pour chaque résolution choisie
-                        for (const resolutionObj of selectedResolutions) {
-                            const { resolution, width } = resolutionObj;
-                            const resizedData = resizeImage(image, width);
-                            imagesData.push({ data: resizedData, resolution });
-                        }
+  if (discord.checked) {
+    checked.push({
+      name: 'discord', 
+      sizes: sizes.discord
+    });
+  }
 
-                        // Vérifie si toutes les images ont été redimensionnées avant de créer le zip
-                        if (imagesData.length === files.length * selectedResolutions.length) {
-                            createZip(imagesData);
-                        }
-                    };
-                };
-                reader.readAsDataURL(file);
-            }
-        }
+  if (moji.checked) {
+    checked.push({
+      name: 'moji',
+      sizes: sizes.moji
+    });
+  }
 
-        // Attache l'événement "click" au bouton "Redimensionner"
-        document.getElementById('resizeButton').addEventListener('click', onResizeButtonClick);
-    </script>
-</body>
-</html>
+  return checked;
+}
+
+// Redimensionner et zipper les images
+function zipImages(files, options) {
+  // Code pour redimensionner et zipper
+}
