a4897974129479ea8170007625bcc74f3ab56649
Dartsgame <80755313+Dartsgame974@users.noreply.github.com>
2023-09-13 20:20:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+// Assurez-vous que les bibliothèques Spotipy et SpotifyWebApi sont incluses
+// Remplacez ces valeurs par vos propres clés d'API
+const client_id = '648744c559e34f23bf849613c3b0f4ca';
+const client_secret = '6108e70bad3a4cf5bb28dab7ada44a5d';
+
 // Fonction pour extraire les titres d'une playlist
 async function extractPlaylistInfo(playlistUrl) {
+    // Initialisez l'objet SpotifyWebApi avec vos clés d'API
+    const spotifyApi = new SpotifyWebApi({
+        clientId: client_id,
+        clientSecret: client_secret
+    });
+
     const response = await fetch(playlistUrl);
     const data = await response.json();
     const tracks = data.tracks.items;
