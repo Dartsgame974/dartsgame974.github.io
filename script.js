611c64a1216325fe6ac21900f11a9cd99175543e
Dartsgame <80755313+Dartsgame974@users.noreply.github.com>
2023-09-13 18:29:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,34 @@
-// Function to compare two Spotify playlists
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotifyApi = new SpotifyWebApi();
+const accessToken = '648744c559e34f23bf849613c3b0f4ca';
+
+spotifyApi.setAccessToken(accessToken);
+
 async function comparePlaylists() {
     const playlist1Link = document.getElementById('playlist1').value;
     const playlist2Link = document.getElementById('playlist2').value;
 
-    // Obtain access tokens for Spotify API (you'll need to implement the authentication flow)
-    const accessToken = '6108e70bad3a4cf5bb28dab7ada44a5d';
+    try {
+        const playlist1 = await spotifyApi.getPlaylist(playlist1Link);
+        const playlist2 = await spotifyApi.getPlaylist(playlist2Link);
 
-    // Get playlist data using the Spotify API
-    const playlist1 = await getPlaylistData(playlist1Link, accessToken);
-    const playlist2 = await getPlaylistData(playlist2Link, accessToken);
+        const tracks1 = extractTracks(playlist1.tracks.items);
+        const tracks2 = extractTracks(playlist2.tracks.items);
 
-    // Extract track names and artists from playlists
-    const tracks1 = extractTracks(playlist1);
-    const tracks2 = extractTracks(playlist2);
+        const commonTracks = findCommonElements(tracks1, tracks2);
+        const commonArtists = findCommonElements(playlist1.artists, playlist2.artists);
 
-    // Find common tracks and artists
-    const commonTracks = findCommonElements(tracks1, tracks2);
-    const commonArtists = findCommonElements(playlist1.artists, playlist2.artists);
-
-    // Display common tracks and artists in the HTML
-    displayResults(commonTracks, commonArtists);
+        displayResults(commonTracks, commonArtists);
+    } catch (error) {
+        console.error('Erreur lors de la récupération des playlists depuis Spotify:', error);
+    }
 }
 
-// Function to retrieve playlist data from Spotify API
-async function getPlaylistData(playlistLink, accessToken) {
-    const playlistId = extractPlaylistId(playlistLink);
-    const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}`, {
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-        },
-    });
-    const data = await response.json();
-    return {
-        name: data.name,
-        artists: extractArtists(data.tracks.items),
-    };
+function extractTracks(tracks) {
+    return tracks.map((track) => track.name);
 }
 
-// Function to extract track names from a playlist
-function extractTracks(playlist) {
-    return playlist.tracks.map((track) => track.name);
-}
-
-// Function to extract artist names from a list of tracks
 function extractArtists(tracks) {
     const artists = new Set();
     for (const track of tracks) {
@@ -53,12 +39,10 @@ function extractArtists(tracks) {
     return Array.from(artists);
 }
 
-// Function to find common elements in two arrays
 function findCommonElements(arr1, arr2) {
     return arr1.filter((element) => arr2.includes(element));
 }
 
-// Function to display common tracks and artists in the HTML
 function displayResults(commonTracks, commonArtists) {
     const commonTracksList = document.getElementById('commonTracks');
     const commonArtistsList = document.getElementById('commonArtists');
